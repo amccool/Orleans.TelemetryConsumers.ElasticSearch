5bf06bbba2b77cf3abf2097da95152019872756c
Alex McCool <alex.mccool@yahoo.com>
2018-07-24 23:29:11 -0700

    prep for unit testing

---
+++ src/ESTester/OrleansTestingBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTester
+{
+    public abstract class OrleansTestingBase
+    {
+        protected static readonly Random random = new Random();
+
+        protected static long GetRandomGrainId()
+        {
+            return random.Next();
+        }
+    }
+}
